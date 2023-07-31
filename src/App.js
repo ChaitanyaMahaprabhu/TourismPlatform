faefd146cde121d56689dd15a0cb83ffe10e4231
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-07-31 17:15:02 +0530

    Custom scrolling created.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { Bold } from './components/Home/Bold';
 import { Footer } from './components/Footer/Footer';
+import { Test } from './components/test';
 
 function App() {
   return (
     <>
-      <Footer/>
     </>
   );
 }
