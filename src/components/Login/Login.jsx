9fac2878bfe92c948ed58a58fb989095dae536d0
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-08-01 16:31:08 +0530

    Login page animation added.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,20 @@
 import styles from "./Login.module.css";
 import { useEffect, useState } from "react";
 const Login = () => {
-  const [image, setImage] = useState(Math.round(Math.random() * 9 + 1));
-
-  useEffect(() => {
-    setTimeout(() => {
-      setImage(Math.round(Math.random() * 9 + 1));
-    }, 10000);
-  });
+  const [gradient, setGradient] = useState(`rgb(${Math.random()*250+1}, ${Math.random()*250+1}, ${Math.random()*250+1}), rgb(${Math.random()*250+1}, ${Math.random()*250+1}, ${Math.random()*250+1})`);
 
   return (
     <div className={styles.loginEncompass}>
-      <div className={styles.decor}>
+      <div className={styles.decor} style = {{background: `linear-gradient(60deg, pink, ${gradient})`}}>
+        <img src = {require('../../assets/images/mountain.png')} className = {styles.mountain}/>
+        <img src = {require('../../assets/images/forest.png')} className = {styles.forest}/>
       </div>
 
-      <div className={styles.form}></div>
+      <div className={styles.form}>
+        <div className={styles.loginForm}>
+            
+        </div>
+      </div>
     </div>
   );
 };
