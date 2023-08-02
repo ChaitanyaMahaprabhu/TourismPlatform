cbebfc7505960fd70a7b7fee0ea6fc2a14d933e9
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-08-02 15:35:05 +0530

    Feedback form integrated to home page.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import styles from './Home.module.css';
 import {Bold} from './Bold';
 import {Reviews} from './Reviews';
 import {Footer} from '../Footer/Footer';
+import {Feedback} from '../Feedback/Feedback';
 
 const Home = () => {
     return(
         <div className = {styles.home}>
-            <Bold className = {styles.bold} id = "bold"/>
-            <Reviews className = {styles.reviews} id = "review"/>
+            <Bold className = {styles.bold}/>
+            <Reviews className = {styles.reviews}/>
+            <Feedback className = {styles.feedback}/>
             <Footer className = {styles.footer}/>
         </div>
     );
