8878424ee3506278c32b4d0b422f2c2cb1444f89
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-08-05 11:58:52 +0530

    Style changes made in gallery.

--- src/components/Gallery/GalleryTitle.jsx
+++ src/components/Gallery/GalleryTitle.jsx
@@ -1,11 +1,27 @@
-import styles from './GalleryTitle.module.css';
+import styles from "./GalleryTitle.module.css";
+import {Link} from "react-router-dom";
 
 const GalleryTitle = () => {
-    return(
-        <div className = {styles.galleryTitleEncompass}>
-            <h1 className={styles.heading}>Gallery</h1>
-        </div>
-    );
-}
+  return (
+    <div className={styles.galleryTitleEncompass}>
+      <h1 className={styles.heading}>Gallery</h1>
+      <h1 className={styles.home}>
+        <Link to="/">
+          <span
+            className="material-symbols-outlined"
+            style={{
+              fontSize: "2rem",
+              color: "white",
+              marginLeft: '1rem',
+              marginTop: '1rem',
+            }}
+          >
+            home
+          </span>
+        </Link>
+      </h1>
+    </div>
+  );
+};
 
-export {GalleryTitle};
+export { GalleryTitle };
